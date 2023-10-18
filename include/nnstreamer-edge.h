690ee08ea49701c23178d65a1ba6a995e0c06b74
hyunil park <hyunil46.park@samsung.com>
2023-10-18 14:16:54 +0900

    [bugfix] Modify dlog output code
    When outputting dlog using dlogutil in Tizen, garbage values are output
    in the format starting with %
    
    Signed-off-by: hyunil park <hyunil46.park@samsung.com>

--- include/nnstreamer-edge.h
+++ include/nnstreamer-edge.h
@@ -75,18 +75,6 @@ typedef enum {
 } nns_edge_node_type_e;
 
 /**
- * @brief Enumeration for log message.
- */
-typedef enum {
-  NNS_EDGE_LOG_DEBUG = 0,
-  NNS_EDGE_LOG_INFO,
-  NNS_EDGE_LOG_WARNING,
-  NNS_EDGE_LOG_ERROR,
-  NNS_EDGE_LOG_FATAL,
-  NNS_EDGE_LOG_NONE
-} nns_edge_log_level_e;
-
-/**
  * @brief Callback for the nnstreamer edge event.
  * @note This callback will suspend data stream. Do not spend too much time in the callback.
  * @return User should return NNS_EDGE_ERROR_NONE if an event is successfully handled.
@@ -487,12 +475,6 @@ int nns_edge_data_get_info (nns_edge_data_h data_h, const char *key, char **valu
  * @retval #NNS_EDGE_ERROR_INVALID_PARAMETER Given parameter is invalid.
  */
 int nns_edge_data_clear_info (nns_edge_data_h data_h);
-
-/**
- * @brief Set the logging level. Default value is NNS_EDGE_LOG_INFO.
- * @param[in] level The log level to print out.
- */
-void nns_edge_set_log_level (nns_edge_log_level_e level);
 
 /**
  * @brief Get the version of nnstreamer-edge.
--- src/libnnstreamer-edge/nnstreamer-edge-log.h
+++ src/libnnstreamer-edge/nnstreamer-edge-log.h
@@ -22,6 +22,35 @@ extern "C" {
 
 #define TAG_NAME "nnstreamer-edge"
 
+#if defined(__TIZEN__)
+#include <dlog.h>
+
+#define nns_edge_logd(...) dlog_print (DLOG_DEBUG, TAG_NAME, __VA_ARGS__)
+#define nns_edge_logi(...) dlog_print (DLOG_INFO, TAG_NAME, __VA_ARGS__)
+#define nns_edge_logw(...) dlog_print (DLOG_WARN, TAG_NAME, __VA_ARGS__)
+#define nns_edge_loge(...) dlog_print (DLOG_ERROR, TAG_NAME, __VA_ARGS__)
+#define nns_edge_logf(...) dlog_print (DLOG_FATAL, TAG_NAME, __VA_ARGS__)
+#elif defined(__ANDROID__)
+#include <android/log.h>
+
+#define nns_edge_logd(...) __android_log_print (ANDROID_LOG_DEBUG, TAG_NAME, __VA_ARGS__)
+#define nns_edge_logi(...) __android_log_print (ANDROID_LOG_INFO, TAG_NAME, __VA_ARGS__)
+#define nns_edge_logw(...) __android_log_print (ANDROID_LOG_WARN, TAG_NAME, __VA_ARGS__)
+#define nns_edge_loge(...) __android_log_print (ANDROID_LOG_ERROR, TAG_NAME, __VA_ARGS__)
+#define nns_edge_logf(...) __android_log_print (ANDROID_LOG_FATAL, TAG_NAME, __VA_ARGS__)
+#else
+/**
+ * @brief Enumeration for log message.
+ */
+typedef enum {
+  NNS_EDGE_LOG_DEBUG = 0,
+  NNS_EDGE_LOG_INFO,
+  NNS_EDGE_LOG_WARNING,
+  NNS_EDGE_LOG_ERROR,
+  NNS_EDGE_LOG_FATAL,
+  NNS_EDGE_LOG_NONE
+} nns_edge_log_level_e;
+
 /**
  * @brief Internal util function to print log message.
  */
@@ -32,7 +61,7 @@ void nns_edge_print_log (nns_edge_log_level_e level, const char *fmt, ...);
 #define nns_edge_loge(...) nns_edge_print_log (NNS_EDGE_LOG_ERROR, __VA_ARGS__)
 #define nns_edge_logd(...) nns_edge_print_log (NNS_EDGE_LOG_DEBUG, __VA_ARGS__)
 #define nns_edge_logf(...) nns_edge_print_log (NNS_EDGE_LOG_FATAL, __VA_ARGS__)
-
+#endif
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
